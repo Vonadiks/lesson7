8cd129422bfe670e77a9c213a84c8088d41a8628
Sergey Skidanov <vonadiks@mail.ru>
2021-04-12 00:56:15 +0400

    lesson7

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,2 +1,31 @@
 public class MyClass {
+    @BeforeSuite
+    public void begin() {
+        System.out.println("begin");
+    }
+
+    @Test(priority = 3)
+    public void test1() {
+        System.out.println("test1 priority 3");
+    }
+
+    @Test(priority = 7)
+    public void test2() {
+        System.out.println("test2 priority 7");
+    }
+
+    @Test(priority = 1)
+    public void test3() {
+        System.out.println("test3 priority 1");
+    }
+
+    @Test(priority = 5)
+    public void test4() {
+        System.out.println("test4 priority 5");
+    }
+
+    @AfterSuite
+    public void end() {
+        System.out.println("end");
+    }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,10 @@
-public class Test {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+    int priority() default 5;
 }
